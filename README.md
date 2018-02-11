c941fbe791a6b7b1dc040afbcb4e4581d865a5da
Dejan Simic <desimic@gmail.com>
2018-02-11 18:26:52 +0100

    Add code and doc

---
+++ README.md
@@ -0,0 +1,15 @@
+# Mazeism
+
+Generate a random maze using Prim's algorithm.
+
+## Install
+
+    go get github.com/terminism/mazeism
+
+## Run
+
+    mazeism
+
+## Demo
+
+![demo](doc/mazeism.gif)
---
+++ main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/nsf/termbox-go"
+)
+
+type Maze struct {
+	in       []Cell
+	out      []Cell
+	frontier []Cell
+}
+
+func NewMaze(width, height int) *Maze {
+	maze := &Maze{}
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			maze.out = append(maze.out, Cell{x: x, y: y})
+		}
+	}
+	return maze
+}
+
+func (m Maze) draw() {
+	for _, c := range m.in {
+		c.draw()
+	}
+}
+
+func (m *Maze) expand(cell *Cell) {
+	for _, c := range m.in {
+		if (c.x == cell.x+1 && c.y == cell.y) ||
+			(c.x == cell.x-1 && c.y == cell.y) ||
+			(c.x == cell.x && c.y == cell.y+1) ||
+			(c.x == cell.x && c.y == cell.y-1) {
+			cell.passages = append(cell.passages, c)
+			break
+		}
+	}
+	m.in = append(m.in, *cell)
+
+	var newOut []Cell
+	for _, c := range m.out {
+		if (c.x == cell.x+1 && c.y == cell.y) ||
+			(c.x == cell.x-1 && c.y == cell.y) ||
+			(c.x == cell.x && c.y == cell.y+1) ||
+			(c.x == cell.x && c.y == cell.y-1) {
+			m.frontier = append(m.frontier, c)
+		} else {
+			newOut = append(newOut, c)
+		}
+	}
+	m.out = newOut
+}
+
+func (m *Maze) step() {
+	if len(m.out)+len(m.frontier) == 0 {
+		return
+	}
+
+	if len(m.frontier) == 0 {
+		n := rand.Intn(len(m.out))
+		cell := m.out[n]
+		m.out = append(m.out[:n], m.out[n+1:]...)
+		m.expand(&cell)
+		return
+	}
+
+	n := rand.Intn(len(m.frontier))
+	cell := m.frontier[n]
+	m.frontier = append(m.frontier[:n], m.frontier[n+1:]...)
+	m.expand(&cell)
+}
+
+type Cell struct {
+	x, y     int
+	passages []Cell
+}
+
+func (c Cell) draw() {
+	termbox.SetCell(c.x*2, c.y*2, ' ', termbox.ColorDefault, termbox.ColorYellow)
+	for _, pc := range c.passages {
+		xoff := pc.x - c.x
+		yoff := pc.y - c.y
+		termbox.SetCell(c.x*2+xoff, c.y*2+yoff, ' ', termbox.ColorDefault, termbox.ColorYellow)
+	}
+}
+
+func init() {
+	rand.Seed(int64(time.Now().Second()))
+}
+
+func main() {
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+
+	w, h := termbox.Size()
+	maze := NewMaze(w/2+1, h/2+1)
+
+	go func() {
+		for {
+			maze.step()
+			time.Sleep(20 * time.Millisecond)
+			maze.draw()
+			termbox.Flush()
+		}
+	}()
+
+	termbox.PollEvent()
+}
